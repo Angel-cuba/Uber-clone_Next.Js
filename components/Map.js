8ae045acf14c12bbd7e74fa4246c362949c2956b
Angel <araozangel842@gmail.com>
2021-11-15 19:31:20 +0200

    Coordenadas, styling y lado de confirmacion

--- components/Map.js
+++ components/Map.js
@@ -15,7 +15,17 @@ const Map = () => {
 			center: [-99.29011, 39.39172], //USA
 			zoom: 3,
 		});
-	});
+		addToMap(map);
+	}, []);
+
+	const addToMap = (map) => {
+		// Create a default Marker and add it to the map.
+		const marker1 = new mapboxgl.Marker().setLngLat([12.554729, 55.70651]).addTo(map);
+		// Create a default Marker, colored black, rotated 45 degrees.
+		const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+			.setLngLat([12.65147, 55.608166])
+			.addTo(map);
+	};
 
 	return <Wrapper id="map"></Wrapper>;
 };
--- pages/search.js
+++ pages/search.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
+import Link from 'next/link';
 
 const Search = () => {
 	return (
 		<Wrapper>
 			<ButtonContainer>
-				<BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+				<Link href="/" passHref>
+					<BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+				</Link>
 			</ButtonContainer>
 
 			<InputContainer>
 				<FromToIcons>
 					<Circle
-						src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png
+						src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png
                "
 					/>
+					<Line src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png" />
+					<Square src="https://img.icons8.com/windows/50/000000/square-full.png" />
 				</FromToIcons>
+
+				<InputBoxes>
+					<Input placeholder="Enter pickup location" />
+					<Input placeholder="Where to....." />
+				</InputBoxes>
+				<PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png" />
 			</InputContainer>
+			<SavedPlaces>
+				<StartIcon src="https://img.icons8.com/ios-filled/50/ffffff/star--v1.png" />
+				Saved places
+			</SavedPlaces>
+
+			<ConfirmationContainer>Confirm Location</ConfirmationContainer>
 		</Wrapper>
 	);
 };
 
 export default Search;
+
+const ConfirmationContainer = tw.div`
+bg-black text-white text-center mt-3 mx-10 py-3 text-2xl cursor-pointer
+`;
 
 const Wrapper = tw.div`
 bg-gray-100 h-screen
@@ -31,10 +52,40 @@ bg-white px-4
 `;
 
 const BackButton = tw.img`
-h-12`;
-const InputContainer = tw.div``;
+h-12 cursor-pointer
+`;
+const InputContainer = tw.div`
+bg-white flex items-center px-4 mb-4
+`;
 
-const FromToIcons = tw.div``;
+const FromToIcons = tw.div`
+w-10 flex flex-col mr-2 items-center
+`;
 
-const Circle = tw.img``;
-const Line = tw.img``;
+const Circle = tw.img`
+h-2.5 
+`;
+const Line = tw.img`
+h-10
+`;
+const Square = tw.img`
+h-3
+`;
+
+const InputBoxes = tw.div`
+flex flex-col flex-1
+`;
+
+const Input = tw.input`
+h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none
+`;
+const PlusIcon = tw.img`
+w-10 h-10 bg-gray-200 rounded-full ml-3`;
+
+const SavedPlaces = tw.div`
+flex items-center bg-white px-4 py-2
+`;
+
+const StartIcon = tw.img`
+bg-gray-400 w-10 h-10 p-2 rounded-full mr-2
+`;
